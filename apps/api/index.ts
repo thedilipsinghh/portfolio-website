e7ccfec70dfda086ec99c72ae9b5b4da24d4a2ea
dilip <dilip@gmail.com>
2026-06-08 11:37:33 +0530

    Remove obsolete Mongoose connection logic

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -2,7 +2,6 @@ import dotenv from "dotenv";
 dotenv.config({});
 
 import express from "express";
-import mongoose from "mongoose";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
@@ -10,8 +9,6 @@ import { FRONTEND_URL } from "./utils/config";
 import adminRoutes from "./route/admin.routes";
 import authRoutes from "./route/auth.routes";
 import emailRoutes from "./route/email.routes";
-
-mongoose.connect(process.env.MONGO_URL as string);
 
 const app = express();
 app.use(express.json());
@@ -29,10 +26,6 @@ app.use(cors({
 app.use("/api/admin", adminRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/email", emailRoutes);
-
-mongoose.connection.once("open", () => {
-    console.log("db connected");
-});
 
 if (!process.env.VERCEL) {
     app.listen(process.env.PORT || 5000, () => {
